80698a780d8fca5ebdc8875d347a6cc4d5678cfd
SarVar <sarvarrozikulov9@gmail.com>
2025-04-08 06:38:57 +0500

    add footer

--- src/components/MainPageFooter.vue
+++ src/components/MainPageFooter.vue
@@ -3,12 +3,132 @@
 </script>
 
 <template>
-<div class="main-footer"></div>
+  <div class="main-footer">
+    <div class="sale-information">
+      <div class="info-item" style="margin-bottom: 15px;">
+        <h1>Итого</h1>
+        <h1 class="value">250000000</h1>
+      </div>
+      <div class="info-item">
+        <h2>Получено</h2>
+        <h2 class="value">120000</h2>
+      </div>
+      <div class="info-item">
+        <h2>Сдача</h2>
+        <h2 class="value">56</h2>
+      </div>
+    </div>
+    <div class="btns">
+      <div class="additional-buttons">
+        <button>Изменить товар</button>
+        <button>Универсальный продукт</button>
+        <button>Быстрые товары</button>
+        <button>отложка</button>
+        <button>доп. фукнкции</button>
+      </div>
+      <div class="finish-delete">
+        <button class="delete">Удалить выбранные</button>
+        <button class="pay">Оплатить</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
 .main-footer {
   background: #e0f2fe;
   padding: 12px;
+  display: flex;
+  align-items: stretch;
+  justify-content: space-between;
+  min-height: 270px;
+
+  button {
+    border: none;
+    background: none;
+    font-size: 20px;
+    border-radius: 8px;
+  }
+
+  .sale-information {
+    display: flex;
+    flex-direction: column;
+    background: #ffffff;
+    border-radius: 8px;
+    border: 1px solid #bae6fd;
+    padding: 20px;
+    color: #444343;
+    min-width: 300px;
+    max-width: 800px;
+
+    .info-item {
+      display: flex;
+      width: 100%;
+      justify-content: space-between;
+
+      h1, h2 {
+        width: 100%;
+      }
+
+      .value {
+        padding-left: 8px
+      }
+    }
+  }
+
+  .btns {
+    display: flex;
+    gap: 8px;
+    max-width: 500px;
+
+    .additional-buttons {
+      display: flex;
+      flex-wrap: wrap;
+      gap: 8px;
+
+      button {
+        background: #ffffff;
+        width: 160px;
+        height: 75px;
+        padding: 10px;
+        border: 1px solid #bae6fd;
+
+        &:hover {
+          background: #f0f9ff;
+        }
+
+        &:active {
+          background: #e0f2fe;
+          color: #0284c7;
+        }
+      }
+    }
+
+    .finish-delete {
+      display: flex;
+      flex-direction: column;
+      gap: 8px;
+
+      .delete {
+        width: 160px;
+        text-align: center;
+        background: #d23050;
+        color: #ffffff;
+        height: 75px;
+        border: 1px solid #ffffff;
+      }
+
+      .pay {
+        background: #43a56a;
+        color: #ffffff;
+        flex: 1;
+        font-size: 28px;
+
+        &:active {
+          background: #51773f;
+        }
+      }
+    }
+  }
 }
 </style>
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -9,11 +9,43 @@ export default {
 </script>
 
 <template>
-  <main-page-header></main-page-header>
-  <router-view></router-view>
-  <main-page-footer></main-page-footer>
+  <div class="main-sales-container">
+    <main-page-header class="header"></main-page-header>
+    <div class="content">
+      <router-view></router-view>
+    </div>
+    <main-page-footer class="footer"></main-page-footer>
+  </div>
 </template>
 
 <style scoped>
+.main-sales-container {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+}
 
+.header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 1000;
+}
+
+.footer {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  z-index: 1000;
+}
+
+.content {
+  margin-top: 65px;
+  margin-bottom: 275px;
+  overflow-y: auto;
+  flex: 1;
+}
 </style>
