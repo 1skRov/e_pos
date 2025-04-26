be5e406d82e633f4c6ff34b4a058a54af85680f1
sarvar <sarvarrozikulov9@gmail.com>
2025-04-26 19:21:04 +0500

    update footer

--- src/components/MainPageFooter.vue
+++ src/components/MainPageFooter.vue
@@ -1,14 +1,14 @@
 <script setup>
-import {ref, h} from 'vue'
-import QuickItemsDrawer from "@/views/Sales/ProductForFastChoice.vue"
-import {useMessage, NAlert} from 'naive-ui'
-import AdditionalFunctions from "@/views/Sales/AdditionalFunctions.vue";
-import Payment from "@/views/Sales/Payment.vue";
+import { ref, h } from 'vue'
+import QuickItemsDrawer from '@/views/Sales/ProductForFastChoice.vue'
+import { useMessage, NAlert } from 'naive-ui'
+import AdditionalFunctions from '@/views/Sales/AdditionalFunctions.vue'
+import Payment from '@/views/Sales/Payment.vue'
 
-const showQuickItems = ref(false);
-const showAdditionalFunctions = ref(false);
-const showPayment = ref(false);
-const {error} = useMessage()
+const showQuickItems = ref(false)
+const showAdditionalFunctions = ref(false)
+const showPayment = ref(false)
+const { error } = useMessage()
 
 const renderErrorAlert = (props) => {
   return h(
@@ -23,52 +23,50 @@ const renderErrorAlert = (props) => {
         maxWidth: 'calc(100vw - 32px)',
         width: '600px',
         lineHeight: '1.6',
-        whiteSpace: 'pre-wrap'
-      }
+        whiteSpace: 'pre-wrap',
+      },
     },
     {
       default: () =>
         `Произошла непредвиденная ошибка при попытке выполнить операцию.
 Пожалуйста, проверьте введённые данные и попробуйте снова.
-Если проблема сохраняется, обратитесь в техподдержку.`
-    }
+Если проблема сохраняется, обратитесь в техподдержку.`,
+    },
   )
 }
 
 function handleExampleMessage() {
   error('', {
     render: renderErrorAlert,
-    closable: true
+    closable: true,
   })
 }
 </script>
 
 <template>
   <div class="main-footer">
-    <div class="sale-information">
-      <div class="info-item" style="margin-bottom: 15px;">
-        <h1>Итого</h1>
-        <h1 class="value">250000</h1>
+    <div class="footer-left">
+      <div class="left-item">
+        <p class="sum">Итого</p>
+        <p class="sum value">200</p>
       </div>
-      <div class="info-item">
-        <h2>Получено</h2>
-        <h2 class="value">120000</h2>
+      <div class="left-item">
+        <p>внесено</p>
+        <p>200</p>
       </div>
-      <div class="info-item">
-        <h2>Сдача</h2>
-        <h2 class="value">56</h2>
+      <div class="left-item">
+        <p>сдача</p>
+        <p>200</p>
       </div>
     </div>
-    <div class="btns">
+    <div class="footer-right">
       <div class="additional-buttons">
-        <button @click="handleExampleMessage">Пример сообщения</button>
-        <button>Закрыть заказ</button>
-        <button @click="showQuickItems = true">Быстрые товары</button>
-        <button>отложка</button>
-        <button @click="showAdditionalFunctions = true">доп. функции</button>
+        <button class="add-btn">Отложка</button>
+        <button class="add-btn" @click="showQuickItems = true">Быстрые товары</button>
+        <button class="add-btn" @click="showAdditionalFunctions = true">Доп. функции</button>
       </div>
-      <div class="finish-delete">
-        <button class="pay" @click="showPayment = true">Оплатить</button>
+      <div class="pay">
+        <button class="pay-btn" @click="showPayment = true">Оплатить</button>
       </div>
     </div>
     <n-drawer v-model:show="showQuickItems" placement="bottom" height="60%">
@@ -93,90 +91,74 @@ function handleExampleMessage() {
 
 <style scoped lang="scss">
 .main-footer {
-  background: #e0f2fe;
-  padding: 12px;
   display: flex;
   align-items: stretch;
+  height: 100%;
+  box-sizing: border-box;
   justify-content: space-between;
-  min-height: 270px;
+  padding: 15px;
+  background: #084661;
+}
 
-  button {
-    border: none;
-    background: none;
-    font-size: 20px;
-    border-radius: 8px;
+.footer-left {
+  padding: 10px;
+  border-radius: 4px;
+  background: #ffffff;
+  min-width: 270px;
+}
+
+.left-item {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 16px;
+  color: #45556c;
+
+  p {
+    font-weight: 500;
+    font-size: 18px;
   }
 
-  .sale-information {
-    display: flex;
-    flex-direction: column;
-    background: #ffffff;
-    border-radius: 8px;
-    border: 1px solid #bae6fd;
-    padding: 20px;
-    color: #444343;
-    min-width: 300px;
-    max-width: 800px;
-
-    .info-item {
-      display: flex;
-      width: 100%;
-      justify-content: space-between;
-
-      h1, h2 {
-        width: 100%;
-      }
-
-      .value {
-        padding-left: 8px;
-      }
-    }
+  .sum {
+    font-size: 28px;
+    font-weight: 600;
   }
-
-  .btns {
-    display: flex;
-    gap: 8px;
-    max-width: 500px;
-
-    .additional-buttons {
-      display: flex;
-      flex-wrap: wrap;
-      gap: 8px;
-
-      button {
-        background: #ffffff;
-        width: 160px;
-        height: 75px;
-        padding: 10px;
-        border: 1px solid #bae6fd;
-
-        &:hover {
-          background: #f0f9ff;
-        }
-
-        &:active {
-          background: #e0f2fe;
-          color: #0284c7;
-        }
-      }
-    }
-
-    .finish-delete {
-      display: flex;
-      gap: 8px;
-
-      .pay {
-        background: #0284c7;
-        color: #ffffff;
-        flex: 1;
-        font-size: 28px;
-        padding: 0 15px;
-
-        &:active {
-          background: #0ea5e9;
-        }
-      }
-    }
+  .value {
+    color: #097f23;
+  }
+}
+.footer-right {
+  display: flex;
+  gap: 15px;
+}
+.additional-buttons {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+.add-btn {
+  border: 1px solid #2591a3;
+  width: 160px;
+  height: 75px;
+  color: #084661;
+  font-weight: 500;
+  border-radius: 4px;
+  font-size: 24px;
+  cursor: pointer;
+}
+.pay-btn {
+  border: 1px solid #2591a3;
+  background: #2591a3;
+  width: 160px;
+  height: 100%;
+  color: #ffffff;
+  font-weight: 500;
+  border-radius: 4px;
+  font-size: 32px;
+  padding: 0 10px;
+  cursor: pointer;
+  &:active {
+    background: #03a0b2;
   }
 }
 </style>
